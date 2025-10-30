e55f9c7fdbab3fd67e36a620e9f470550515da48
Keila-dev <keilacristina.florindo@gmail.com>
2025-10-30 13:25:28 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿
+namespace MauiAppHotel //nome do arquivo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400; //largura
+            window.Height = 600; //altura
+            return window;
+        }
+    }
+}
