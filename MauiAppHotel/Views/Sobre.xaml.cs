5bcd35ade21e4704377eecb705713fa85f2d62e8
Keila-dev <keilacristina.florindo@gmail.com>
2025-11-03 11:28:04 -0300

    Funcionamento data

--- MauiAppHotel/Views/Sobre.xaml.cs
+++ MauiAppHotel/Views/Sobre.xaml.cs
@@ -8,5 +8,18 @@ namespace MauiAppHotel.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PopAsync();
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
     }
 }
